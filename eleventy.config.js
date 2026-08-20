bd31a7c8b5989ddb8c8a5a4ccbca6cebdabb7e0b
Oscarthefish <oscarthephish@gmail.com>
2026-08-21 09:08:29 +1200

    Initial Eleventy project scaffold

---
+++ eleventy.config.js
@@ -0,0 +1,14 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+}
